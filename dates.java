eb67a793d2c2c45d90747eebaa79eb8e95c60606
stabe145 <stabe145@student.otago.ac.nz>
2022-03-10 10:31:07 +1300

    deleted project

--- dates.java
+++ dates.java
@@ -1,32 +1,14 @@
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.util.Scanner;
 
 public class dates{
 
     public static void main(String[]args){
-        System.out.println(isValidDate("20-01-2014"));
-        System.out.println(isValidDate("11-04-2015"));
-        System.out.println(isValidDate("32476347656435"));
+        Scanner scan = new Scanner(System.in);
+        String date = scan.nextLine();
+        isValidDate(date);
     }
 
-    public static boolean isValidDate(String inDate) {
-        String[] dateFormats = {"dd-mm-yyyy", "0d-mm-yyyy", "d-mm-yyyy", "0d-0m-yyyy", "dd-0m-yyyy", "d-0m-yyyy",
-            "d-m-yyyy", "0d-m-yyyy", "dd-m-yyyy", "dd-mm-yy", "0d-mm-yy", "d-mm-yy", "0d-0m-yy", "dd-0m-yy", "d-0m-yy", 
-            "d-m-yy", "0d-m-yy", "dd-m-yy", "dd/mm/yyyy", "0d/mm/yyyy", "d/mm/yyyy", "0d/0m/yyyy", "dd/0m/yyyy", "d/0m/yyyy", 
-            "d/m/yyyy", "0d/m/yyyy", "dd/m/yyyy", "dd/mm/yy", "0d/mm/yy", "d/m/yy", "0d/0m/yy", "dd/0m/yy", "d/0m/yy", "d/m/yy", "0d-m/yy", 
-            "dd/m/yy", "dd mm yyyy", "0d mm yyyy ", "d mm yyyy", "0d 0m yyyy", "dd 0m yyyy", "d 0m yyyy", 
-            "d m yyyy", "0d m yyyy", "dd m yyyy", "dd mm yy", "0d mm yy", "d mm yy", "0d 0m yy", "dd 0m yy", "d 0m yy", "d m yy", "0d m yy", 
-            "dd m yy"};
-        for(int i = 0; i < dateFormats.length; i++){
-            SimpleDateFormat dateFormat = new SimpleDateFormat(dateFormats[i]);
-            dateFormat.setLenient(false);
-            try {
-                dateFormat.parse(inDate.trim());
-            } catch (ParseException pe) {
-                System.out.println("err");
-            }
-            return true;
-        }
-      return true;
+    public static void isValidDate(String inDate) {
+        
     }
 }
